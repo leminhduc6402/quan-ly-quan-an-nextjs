2fd8859659f643391547f49f60752a39c717a4dc
leminhduc6402 <leminhduc6402@gmail.com>
2025-09-08 21:01:30 +0700

    Apply middleware to authenticated

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const privatePaths = ["/manage"];
+const unAuthPaths = ["/login", "/signup"];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const isAuth = Boolean(request.cookies.get("accessToken")?.value);
+
+
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/manage/:path*", "/login", "/signup"],
+};
