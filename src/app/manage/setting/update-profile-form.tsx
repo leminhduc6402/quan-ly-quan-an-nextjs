80ac649226ba02c768d3cb84efe591e7387db974
leminhduc6402 <leminhduc6402@gmail.com>
2025-09-09 18:29:44 +0700

    update personal profile

--- src/app/manage/setting/update-profile-form.tsx
+++ src/app/manage/setting/update-profile-form.tsx
@@ -14,15 +14,18 @@ import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useUploadMediaMutation } from "@/queries/useMedia";
 import { useEffect, useRef, useState } from "react";
-import { useAccountProfile } from "@/queries/useAccount";
+import { useAccountProfile, useUpdateMeMutation } from "@/queries/useAccount";
+import { handleErrorApi } from "@/lib/utils";
+import { toast } from "sonner";
 
 export default function UpdateProfileForm() {
   const [file, setFile] = useState<File | null>(null);
   const avatarInputRef = useRef<HTMLInputElement>(null);
 
   const uploadMediaMutation = useUploadMediaMutation();
+  const updateMeMutation = useUpdateMeMutation();
 
-  const { data } = useAccountProfile();
+  const { data, refetch } = useAccountProfile();
   const form = useForm<UpdateMeBodyType>({
     resolver: zodResolver(UpdateMeBody),
     defaultValues: {
@@ -45,11 +48,37 @@ export default function UpdateProfileForm() {
   const name = form.watch("name");
   const previewAvatar = file ? URL.createObjectURL(file) : avatar;
 
+  const reset = () => {
+    form.reset();
+  };
+  const onSubmit = async (data: UpdateMeBodyType) => {
+    if (updateMeMutation.isPending) return;
+    try {
+      let body = data;
+      if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        const uploadAvatarResult = await uploadMediaMutation.mutateAsync(
+          formData
+        );
+        const imageUrl = uploadAvatarResult.payload.data;
+        body = { ...data, avatar: imageUrl };
+        const result = await updateMeMutation.mutateAsync(body);
+
+        if (result.status === 200) toast.success("Cập nhật thành công");
+        else toast.error("Cập nhật thất bại");
+        refetch();
+      }
+    } catch (error) {
+      handleErrorApi({ error, setError: form.setError });
+    }
+  };
   return (
     <Form {...form}>
       <form
         noValidate
         className="grid auto-rows-max items-start gap-4 md:gap-8"
+        onSubmit={form.handleSubmit(onSubmit)}
       >
         <Card x-chunk="dashboard-07-chunk-0">
           <CardHeader>
@@ -78,8 +107,9 @@ export default function UpdateProfileForm() {
                           const selectedFile = e.target.files?.[0] || null;
                           if (selectedFile) {
                             setFile(selectedFile);
-                            // const formData = new FormData();
-                            // formData.append("file", selectedFile);
+                            field.onChange(
+                              "http://localhost:3000/" + field.value
+                            );
                           }
                         }}
                       />
@@ -116,8 +146,13 @@ export default function UpdateProfileForm() {
               />
 
               <div className=" items-center gap-2 md:ml-auto flex">
-                <Button variant="outline" size="sm" type="reset">
-                  Hủy
+                <Button
+                  variant="outline"
+                  size="sm"
+                  type="reset"
+                  onClick={reset}
+                >
+                  Đặt lại
                 </Button>
                 <Button size="sm" type="submit">
                   Lưu thông tin
--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -1,10 +1,16 @@
 import accountApiRequest from "@/apiRequests/account";
 import { AccountResType } from "@/schemaValidations/account.schema";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useAccountProfile = () => {
   return useQuery({
     queryKey: ["account-profile"],
     queryFn: accountApiRequest.me,
+  });
+};
+
+export const useUpdateMeMutation = () => {
+  return useMutation({
+    mutationFn: accountApiRequest.updateMe,
   });
 };
