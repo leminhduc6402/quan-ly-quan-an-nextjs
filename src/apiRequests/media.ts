fd119fbe35c042f9cb1f8e61f1a4243ebf03b7ea
leminhduc6402 <leminhduc6402@gmail.com>
2025-09-09 17:36:55 +0700

    perform avatar and name

---
+++ src/apiRequests/media.ts
@@ -0,0 +1,13 @@
+import http from "@/lib/http";
+import { UploadImageResType } from "@/schemaValidations/media.schema";
+
+export const mediaApiRequests = {
+  upload: (formData: FormData) =>
+    http.post<UploadImageResType>(
+      "/media/upload",
+      formData
+      //     , {
+      //   headers: { "Content-Type": "multipart/form-data" },
+      // }
+    ),
+};
--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -1,9 +1,10 @@
 import accountApiRequest from "@/apiRequests/account";
+import { AccountResType } from "@/schemaValidations/account.schema";
 import { useQuery } from "@tanstack/react-query";
 
 export const useAccountProfile = () => {
   return useQuery({
     queryKey: ["account-profile"],
-    queryFn: () => accountApiRequest.me(),
+    queryFn: accountApiRequest.me,
   });
 };
