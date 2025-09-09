c8fd6b59fd38aa6c8c0fe907841ea4e66a2302c0
leminhduc6402 <leminhduc6402@gmail.com>
2025-09-09 14:20:04 +0700

    Logic logout

--- src/app/(public)/(auth)/login/login-form.tsx
+++ src/app/(public)/(auth)/login/login-form.tsx
@@ -16,8 +16,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useLoginMutation } from "@/queries/useAuth";
 import { toast } from "sonner";
 import { handleErrorApi } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
+  const router = useRouter();
   const loginMutation = useLoginMutation();
   const form = useForm<LoginBodyType>({
     resolver: zodResolver(LoginBody),
@@ -32,7 +34,10 @@ export default function LoginForm() {
     }
     try {
       const result = await loginMutation.mutateAsync(data);
-      if (result.status === 200) toast.success("Đăng nhập thành công");
+      if (result.status === 200) {
+        toast.success("Login success");
+        router.push("/manage/dashboard");
+      }
     } catch (error) {
       handleErrorApi({
         error,
