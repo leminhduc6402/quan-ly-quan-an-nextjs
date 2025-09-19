8f27b7e9c18072811dd9d14265ef914157b805fc
leminhduc6402 <leminhduc6402@gmail.com>
2025-09-19 14:28:32 +0700

    draw QR code

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -6,23 +6,45 @@ import { useEffect, useRef } from "react";
 export default function QRCodeTable({
   token,
   tableNumber,
-  width = 100,
+  width = 250,
 }: {
   token: string;
   tableNumber: number;
   width?: number;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
   useEffect(() => {
-    var canvas = canvasRef.current;
+    var canvas = canvasRef.current!;
+    canvas.width = width;
+    canvas.height = width + 70;
+    const canvasContext = canvas.getContext("2d")!;
+    canvasContext.fillStyle = "white";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.font = "20px Arial";
+    canvasContext.textAlign = "center";
+    canvasContext.fillStyle = "black";
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    );
+    canvasContext.fillText(
+      `Quét mã để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    );
+    const virtualCanvas = document.createElement("canvas");
     QRCode.toCanvas(
-      canvas,
+      virtualCanvas,
       getTableLink({ token, tableNumber }),
       function (error) {
         if (error) console.error(error);
+        canvasContext.drawImage(virtualCanvas, 0, 0, width, width);
       }
     );
-  }, []);
+  }, [token, tableNumber, width]);
+
   return (
     <div>
       <canvas ref={canvasRef} />
